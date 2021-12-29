87a3e9ab8b633f5c17f0edda51f8b5d63d11d512
bassima_gh <ghba20wu@stident.ju.se>
2021-12-29 21:10:45 +0100

    finished the sliding login and sign up form

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ var moon_icon = document.getElementById("moon_svg");
 moon_icon.onclick = function() {
     document.body.classList.toggle("dark_mode");
 }
+
+// animation 
+
+const signUpButton = document.getElementById('signUp');
+const logInButton = document.getElementById('logIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+logInButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
